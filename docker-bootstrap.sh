0560729fde8c1831dd7234b5dd4716726f4e9806
Denis Francia <dsfkarell@gmail.com>
2026-05-07 14:46:57 -0400

    -- Añadido explícitamente la variable con la ruta de la base de datos al punto de entrada que levanta el proyecto en Docker.

--- docker-bootstrap.sh
+++ docker-bootstrap.sh
@@ -4,10 +4,10 @@
 set -e
 
 echo "Aplicando migraciones de Prisma..."
-npx prisma migrate deploy
+DATABASE_URL="$DATABASE_URL" npx prisma migrate deploy
 
 echo "Poblando la base de datos (seeding)..."
-npx prisma db seed
+DATABASE_URL="$DATABASE_URL" npx prisma db seed
 
 echo "Iniciando la aplicación NestJS..."
 exec node dist/main
