1f9e6d4127cf5e9eff0b370f911870376d803811
Denis Francia <dsfkarell@gmail.com>
2026-05-20 18:41:53 -0400

    -- Cambios en el docker-bootstrap.sh

--- docker-bootstrap.sh
+++ docker-bootstrap.sh
@@ -7,7 +7,7 @@ echo "Aplicando migraciones de Prisma..."
 DATABASE_URL="$DATABASE_URL" npx prisma migrate deploy
 
 #echo "Poblando la base de datos (seeding)..."
-#Desactivado para producción. Si necesitas hacer seed, hazlo manualmente con el siguiente comando: docker-compose exec backend npx ts-node src/prisma/seed.ts
+#Desactivado para producción. Si necesitas hacer seed, hazlo manualmente con el siguiente comando: docker-compose exec backend node dist/src/prisma/seed.js
 #DATABASE_URL="$DATABASE_URL" npx prisma db seed
 
 echo "Iniciando la aplicación NestJS..."
